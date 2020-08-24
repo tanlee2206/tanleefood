a12242ae00b2105de13392f5a7870c3763af34b1
huỳnh tân <lyhuynhtan2206@gmail.com>
2020-08-24 11:04:43 +0700

    faker food

--- database/factories/FoodFactory.php
+++ database/factories/FoodFactory.php
@@ -5,21 +5,22 @@
 use App\Food;
 use Faker\Generator as Faker;
 
-// $factory->define(Food::class, function (Faker $faker) {
-//     return [
-//         'name'=>$faker->sentence(2),
-//         'price'=>$faker->numberBetween(100, 5000),
-//         'description' => $faker->sentence(10),
-
-
-//     ];
-// });
-$fakerVN = \Faker\Factory::create('vi_VN');
-$factory->define(Food::class, function (Faker $faker) use ($fakerVN) {
+$factory->define(Food::class, function (Faker $faker) {
+    $faker->addProvider(new \FakerRestaurant\Provider\en_US\Restaurant($faker));
     return [
-        'name'        => $fakerVN->sentence(3),
-        'price'=> $faker->numberBetween(100, 5000),
-        'description' => $fakerVN->paragraph(4),
-       
+        'name'=>$faker->foodName(),
+        'price'=>$faker->numberBetween(100, 5000),
+        'description' => $faker->sentence(10),
+
+
     ];
 });
+// $fakerVN = \Faker\Factory::create('vi_VN');
+// $factory->define(Food::class, function (Faker $faker) use ($fakerVN) {
+//     return [
+//         'name'        => $fakerVN->name,
+//         'price'=> $faker->numberBetween(100, 5000),
+//         'description' => $fakerVN->paragraph(4),
+       
+//     ];
+// });
