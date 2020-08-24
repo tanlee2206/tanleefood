9a5440f41fa579bdc1123c11daad6230aa125ffa
huỳnh tân <lyhuynhtan2206@gmail.com>
2020-08-24 22:54:17 +0700

    factory food

--- database/factories/FoodFactory.php
+++ database/factories/FoodFactory.php
@@ -5,19 +5,19 @@
 use App\Food;
 use Faker\Generator as Faker;
 
-$factory->define(Food::class, function (Faker $faker) {
-    $faker->addProvider(new \FakerRestaurant\Provider\vi_VN2\Restaurant($faker));
-    return [
-        'name'=>$faker->foodName(),
-        'price'=>$faker->numberBetween(100, 5000),
-        'description' => $faker->sentence(10),
-    ];
-});
 // $factory->define(Food::class, function (Faker $faker) {
-//     $faker->addProvider(new \FakerRestaurant\Provider\en_US\Restaurant($faker));
+//     $faker->addProvider(new \FakerRestaurant\Provider\vi_VN2\Restaurant($faker));
 //     return [
 //         'name'=>$faker->foodName(),
 //         'price'=>$faker->numberBetween(100, 5000),
 //         'description' => $faker->sentence(10),
 //     ];
 // });
+$factory->define(Food::class, function (Faker $faker) {
+    $faker->addProvider(new \FakerRestaurant\Provider\en_US\Restaurant($faker));
+    return [
+        'name'=>$faker->foodName(),
+        'price'=>$faker->numberBetween(100, 5000),
+        'description' => $faker->sentence(10),
+    ];
+});
