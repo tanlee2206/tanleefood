d5e5071ea2ce1165360e96ad0174cab38360237f
huỳnh tân <lyhuynhtan2206@gmail.com>
2020-09-08 10:36:35 +0700

    fix seed database

--- database/seeds/ShopSeeder.php
+++ database/seeds/ShopSeeder.php
@@ -11,18 +11,19 @@ class ShopSeeder extends Seeder
      */
     public function run()
     {
-        // factory(App\Shop::class,2)->create();
         DB::table('shop')->insert([
             'name'=>'Quán Nguyễn Vũ',
             'description' => 'You may use the once method to log a user into the application for a single request. No sessions or cookies will be utilized, which means this method may be',
             'user_id'=>'3',
             'address_id'=>'1',
+            'img'=>'asset/customer/images/product-1.jpg',
             ]);
          DB::table('shop')->insert([
             'name'=>'Quán Cơm Bà Bãy',
             'description' => 'You may use the once method to log a user into the application for a single request. No sessions or cookies will be utilized, which means this method may be',
             'user_id'=>'4',
             'address_id'=>'2',
+            'img'=>'asset/customer/images/product-2.jpg',
             ]);
     }
 }
